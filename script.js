1b4ddcec7484ac489cf77bcb4fd18dc132e4be2d
lllsshhh <qaz31378@gmail.com>
2025-07-23 16:53:09 +0900

    노래넣음

--- script.js
+++ script.js
@@ -10,20 +10,105 @@ const videos = [
     videoId: "nfs8NYg7yQM"
   },
   {
-    title: "권진아 - 위로",
+    title: "All day project - Famous",
     genre: "K-POP",
-    videoId: "iHrbq8DWm24"
+    videoId: "Xl4D6pZZLgByXYiU"
   },
   {
-    title: "Lofi Hip-hop Chill Mix",
+    title: "언에듀 - 룰루랄라",
     genre: "Hip-hop",
-    videoId: "5qap5aO4i9A"
+    videoId: "sv6WvFNoDJxAMiuu"
   },
   {
-    title: "Jazz Vibes for Relaxing",
-    genre: "Jazz",
-    videoId: "Dx5qFachd3A"
-  }
+    title: "Ph-1 - Good day",
+    genre: "Hip-hop",
+    videoId: "ZPHrHhq2OBjdxOk5"
+  },
+  {
+    title: "호미들 - 사임쌓임",
+    genre: "Hip-hop",
+    videoId: "bKFZ2i0gM7Hu7HMM"
+  },
+  {
+    title: "버즈 - My Love",
+    genre: "발라드",
+    videoId: "KPKJjycZB7f4fTjn"
+  },
+  {
+    title: "우원재 - 시",
+    genre: "Hip-hop",
+    videoId: "GLSwywT0XdlXEvE8"
+  },
+  {
+    title: "포스트맨 - 신촌을 못",
+    genre: "발라드",
+    videoId: "IQChTs4y56INVenW"
+  },
+  {
+    title: "Post Malone, Swae Lee - Sunflower",
+    genre: "Pop",
+    videoId: "1lX6I-aVtakkkPGk"
+  },
+  {
+    title: "NewJeans - OMG",
+    genre: "K-POP",
+    videoId: "9VYshORkeZxvieDz"
+  },
+  {
+    title: "V.O.S - 울어",
+    genre: "발라드",
+    videoId: "A6RyOiWjIoL8TMNH"
+  },
+  {
+    title: "The Kid LAROI, Justin Bieber - STAY",
+    genre: "Pop",
+    videoId: "Jj6QSfYbfv0qX8b9"
+  },
+   {
+    title: "창모 - 메테오",
+    genre: "Hip-hop",
+    videoId: "k64pu8pgfsxlMLtf"
+  },
+  {
+    title: "NewJeans - ETA",
+    genre: "K-POP",
+    videoId: "FSiyJBLJuGMperVO"
+  },
+  {
+    title: "Aespa - Whiplash",
+    genre: "K-POP",
+    videoId: "-m_lynjb9wIJkUcj"
+  },
+  {
+    title: "Potty Monkey - 남자니까",
+    genre: "Hip-hop",
+    videoId: "nYJs_ZwRN5VR_gH9"
+  },
+   {
+    title: "오혁 - 소녀",
+    genre: "발라드",
+    videoId: "OSR3-U7dRX_7RNKz"
+  },
+  {
+    title: "카더가든 - 가까운 듯 먼 그대여",
+    genre: "발라드",
+    videoId: "6J08qJSxNaxvyf9P"
+  },
+  {
+    title: "더 넛츠 - 사랑의 바보",
+    genre: "발라드",
+    videoId: "Tr2zh3LQLIIPiA1v"
+  },
+  {
+    title: "Clean Bandit - Symphony",
+    genre: "Pop",
+    videoId: "yvq1VmE1aPG6l_b2"
+  },
+   {
+    title: "Wiz Khalifa - See You Again ",
+    genre: "Pop",
+    videoId: "o8hMCKOZxQGv2OjA"
+  },
 ];
 
 const searchInput = document.getElementById("searchInput");
